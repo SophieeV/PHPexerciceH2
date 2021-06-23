a1ca34b5b2ca5856290e3e85539fb3f28ee83e61
SophieeV <sophievesin@wanadoo.fr>
2021-06-23 16:16:56 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExerciceH2</title>
+</head>
+<body>
+<?php
+$_SESSION['prenom'] = 'Jane';
+$_SESSION['nom'] = 'Doe';
+
+
+echo 'bonjour '.$_SESSION['prenom']. ' ' .$_SESSION['nom']; ?>
+
+</body>
+</html>
